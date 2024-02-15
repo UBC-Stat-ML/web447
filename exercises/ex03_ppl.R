3ad3fdfaf1c3a4e6446a7fc4a732aebc94e26d59
Alexandre Bouchard <bouchard@stat.ubc.ca>
2024-02-15 11:02:09 -0800

    Built site for gh-pages

---
+++ exercises/ex03_ppl.R
@@ -0,0 +1,15 @@
+coin_flips = rep(0, 4) # "dataset" of four identical coin flips = (0, 0, 0, 0) 
+
+# simPPLe's description of our "bag of coins" example
+my_first_probabilistic_program = function() {
+  
+  # Similar to forward sampling, but use 'observe' when the variable is observed
+  coin_index = simulate(DiscreteDistribution(supp = 0:2))
+  for (i in seq_along(coin_flips)) { 
+    prob_heads = coin_index/2
+    observe(coin_flips[i], Bern(1 - prob_heads)) 
+  }
+  
+  # return the test function g(x, y)
+  return(ifelse(coin_index == 1, 1, 0))
+}
