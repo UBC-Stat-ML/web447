c088384156d15c60e4495f5cd3f7d21a9f721e35
nikola-sur <nikola.surjanovic@stat.ubc.ca>
2025-03-29 12:09:01 -0700

    new homework 9

--- exercises/ex09_q1_template.R
+++ exercises/ex09_q1_template.R
@@ -1,19 +1,19 @@
 set.seed(1)
 
-log_joint = function(rates, change_point, y) {
+log_joint = function(means, change_point, y) {
   
   # Return log(0.0) if parameters are outside of the support
-  if (rates[[1]] < 0 | rates[[2]] < 0 | change_point < 1 | change_point > length(y)) 
+  if (means[[1]] < 0 | means[[2]] < 0 | means[[1]] > 1 | means[[2]] > 1 | change_point < 1 | change_point > length(y)) 
     return(-Inf)
   
   log_prior = 
-    dexp(rates[[1]], 1/100, log = TRUE) + 
-    dexp(rates[[2]], 1/100, log = TRUE)
+    dunif(means[[1]], 0, 1, log = TRUE) + 
+    dunif(means[[2]], 0, 1, log = TRUE)
   
   log_likelihood = 0.0
   for (i in 1:length(y)) {
-    rate = if (i < change_point) rates[[1]] else rates[[2]]
-    log_likelihood = log_likelihood + dpois(y[[i]], rate, log = TRUE)
+    mean = if (i < change_point) means[[1]] else means[[2]]
+    log_likelihood = log_likelihood + dbeta(y[[i]]/5, mean*5, (1-mean)*5, log = TRUE)
   }
   
   return(log_prior + log_likelihood)
--- exercises/ex09_q2_template.R
+++ exercises/ex09_q2_template.R
@@ -2,10 +2,10 @@ forward_posterior = function(synthetic_data_size, n_mcmc_iters) {
   initial = forward(synthetic_data_size)
   
   if (n_mcmc_iters > 0) {
-    samples = mcmc(initial$rates, initial$change_point, initial$data, n_mcmc_iters)
-    return(samples$last_iteration_rates[[1]])
+    samples = mcmc(initial$means, initial$change_point, initial$data, n_mcmc_iters)
+    return(samples$last_iteration_means[[1]])
   } else {
-    return(initial$rates[[1]])
+    return(initial$means[[1]])
   }
 }
 
